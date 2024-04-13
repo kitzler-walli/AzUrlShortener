afac701f72fd92640e7dd3e997e41cff19003a78
Stephan Kitzler-Walli <stephan@kw.at>
2024-04-13 18:18:28 +0200

    add Keep Alive

---
+++ src/Cloud5mins.ShortenerTools.Functions/Functions/KeepAlive.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Cloud5mins.ShortenerTools.Functions.Functions
+{
+    public class KeepAlive
+    {
+        private readonly ILogger<KeepAlive> _logger;
+
+        public KeepAlive(ILogger<KeepAlive> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("KeepAlive")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Keep Alive was executed successfully!");
+        }
+    }
+}
