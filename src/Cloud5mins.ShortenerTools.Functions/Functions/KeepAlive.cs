34ca1c8b18e475786bdb533ee2376e8f012ebcfb
Stephan Kitzler-Walli <stephan@kw.at>
2024-04-13 18:28:57 +0200

    fix route for keep alive

--- src/Cloud5mins.ShortenerTools.Functions/Functions/KeepAlive.cs
+++ src/Cloud5mins.ShortenerTools.Functions/Functions/KeepAlive.cs
@@ -15,7 +15,7 @@ namespace Cloud5mins.ShortenerTools.Functions.Functions
         }
 
         [Function("KeepAlive")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "api/KeepAlive")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Keep Alive was executed successfully!");
